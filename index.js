b0255a4b0722a9502b99af8a2858b9c980e20033
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-10-28 22:07:20 +0700

    setup router, controller and grouping endpoint

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const router = require('./src/routes');
 
 const app = express();
 
 app.use(express.json());
+
+// grouping router endpoint
+app.use('/api/v1/', router);
 
 const port = 5000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
