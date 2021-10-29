f59c63bfb7623caf09902ed123bf231b9309074d
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-10-29 23:32:42 +0700

    done country

---
+++ src/controller/countryController.js
@@ -0,0 +1,120 @@
+const { Country } = require('../../models');
+
+exports.addCountry = async (req, res) => {
+  try {
+    const data = req.body;
+    await Country.create(data);
+
+    res.send({
+      status: 'success',
+      message: 'add country success',
+      data: data,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'failed',
+      message: 'server error',
+    });
+  }
+};
+
+exports.getAllCountry = async (req, res) => {
+  try {
+    const dataCountry = await Country.findAll({
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+
+    res.send({
+      message: 'Get all data success',
+      data: dataCountry,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'failed',
+      message: 'server error',
+    });
+  }
+};
+
+exports.getCountry = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const dataCountry = await Country.findOne({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+    res.send({
+      message: 'Get data success',
+      data: dataCountry,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'failed',
+      message: 'server error',
+    });
+  }
+};
+
+exports.updateCountry = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Country.update(req.body, {
+      where: {
+        id,
+      },
+    });
+
+    const updatedData = await Country.findOne({
+      where: {
+        id,
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+      },
+    });
+
+    res.send({
+      message: 'update data country is successfull',
+      data: {
+        country: updatedData,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'failed',
+      message: 'server error',
+    });
+  }
+};
+
+exports.deleteCountry = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Country.destroy({
+      where: {
+        id,
+      },
+    });
+    res.send({
+      status: 'success',
+      message: `delete country with id: ${id} success`,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      status: 'failed',
+      message: 'server error',
+    });
+  }
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,20 @@ const router = express.Router();
 
 // import controller
 const { addUser, getUsers, getUser, updateUser, deleteUser } = require('../controller/userController');
+const { addCountry, getAllCountry, getCountry, updateCountry, deleteCountry } = require('../controller/countryController');
 
-// route
+// routes user
 router.post('/users', addUser);
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
 router.put('/users/:id', updateUser);
 router.delete('/users/:id', deleteUser);
+
+// routes country
+router.post('/country', addCountry);
+router.get('/country', getAllCountry);
+router.get('/country/:id', getCountry);
+router.put('/country/:id', updateCountry);
+router.delete('/country/:id', deleteCountry);
 
 module.exports = router;
