42eb3990aeeb3bfd18ac0d1642a19b92370a2a5c
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-10-30 23:12:59 +0700

    add jwt in response register

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const router = require('./src/routes');
 
--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,6 +1,7 @@
 const { User } = require('../../models');
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.register = async (req, res) => {
   // create validation scheme with joi
@@ -22,9 +23,11 @@ exports.register = async (req, res) => {
   }
 
   try {
+    // hashing password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
+    // checking user exist by email
     const userExist = await User.findOne({
       where: {
         email: req.body.email,
@@ -38,12 +41,17 @@ exports.register = async (req, res) => {
       });
     }
 
+    // create new user
     const newUser = await User.create({
       ...req.body,
       password: hashedPassword,
       role: 'user',
     });
 
+    // generate token
+    const token = jwt.sign({ id: newUser.id, role: newUser.role }, process.env.TOKEN_KEY);
+
+    // response
     res.send({
       status: 'success',
       message: 'register user is successfull',
@@ -52,6 +60,7 @@ exports.register = async (req, res) => {
         email: newUser.email,
         phone: newUser.phone,
         address: newUser.address,
+        token,
       },
     });
   } catch (error) {
