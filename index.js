3d3c7252c1c74f515dd1abee6e0fb1b23ba52195
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-03 23:30:36 +0700

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const router = require('./src/routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // grouping router endpoint
 app.use('/api/v1/', router);
